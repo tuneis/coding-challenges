85b11c9c584fe864317669daac3efaa4a6ab4633
Ryan Tunis <rtunis@niagaracollege.ca>
2018-08-01 19:55:00 -0400

    Added incrementer and some commenting.

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Diagnostics;
 
 namespace CodingChallenges
 {
     class Program
     {
+        /// <summary>
+        /// Entry point to application.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Debug.WriteLine(Challenges.FirstReverse("Reverse This!"));
-            Debug.WriteLine(Challenges.FirstFactorial(8));
-            Debug.WriteLine(Challenges.LongestWord("I love dogs acre"));
+            // incrementer
+            int i = 1;
+
+            // Challenge 1
+            Console.WriteLine($"Challenge {i++}: {Challenges.FirstReverse("Reverse This!")}");
+
+            // Challenge 2
+            Console.WriteLine($"Challenge {i++}: {Challenges.FirstFactorial(8)}");
+
+            // Challenge 3
+            Console.WriteLine($"Challenge {i++}: {Challenges.LongestWord("I love dogs acre")}");
+
+            // wait to exit until user key press
+            Console.ReadKey();
         }
     }
 }
